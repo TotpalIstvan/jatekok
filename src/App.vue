8429dd71f1b40405a889f13b74ce5148ba50e8b1
TotpalIstvan <totpal.istvan@petrik.hu>
2021-12-23 18:14:11 +0100

    alap

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <table border="1">
+    <thead>
+      <tr>
+      <th>Név</th>
+      <th>Kiadási év</th>
+      <th>Kiadó</th>
+      <th>Free-to-play?</th>
+      </tr>
+    </thead>
+    <tbody>
+    <tr>
+      <td>Far Cry 6</td>
+      <td>2021</td>
+      <td>Ubisoft</td>
+      <td>nem</td>
+    </tr>
+    <tr>
+      <td>World of Tanks</td>
+      <td>2010</td>
+      <td>Wargaming</td>
+      <td>igen</td>
+    </tr>
+    <tr>
+      <td>Simcity</td>
+      <td>2013</td>
+      <td>Electronic Arts</td>
+      <td>nem</td>
+    </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+  data() {
+      return {
+        rows: [
+            {
+              nev: 'Far Cry 6',
+              kiadasi_ev: 2021,
+              kiado: 'Ubisoft',
+              free_to_play: false
+            },
+
+            {
+              nev: 'World of Tanks',
+              kiadasi_ev: 2010,
+              kiado: 'Wargaming',
+              free_to_play: true
+            },
+
+            {
+              nev: 'Simcity',
+              kiadasi_ev: 2013,
+              kiado: 'Electronic Arts',
+              free_to_play: false
+            }
+        ]
+      }
   }
 }
 </script>
