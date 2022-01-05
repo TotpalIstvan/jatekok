6d1935a6d31aea205cf6f0ec562430bf4b04376b
TotpalIstvan <totpal.istvan@petrik.hu>
2022-01-05 22:12:19 +0100

    rows kivétele a root komponensből

--- src/App.vue
+++ src/App.vue
@@ -12,33 +12,6 @@ export default {
   name: 'App',
   components: {
     Tablazat
-  },
-  data() {
-return {
-   rows: [
-            {
-              nev: 'Far Cry 6',
-              kiadasi_ev: 2021,
-              kiado: 'Ubisoft',
-              free_to_play: 'nem'
-            },
-
-            {
-              nev: 'World of Tanks',
-              kiadasi_ev: 2010,
-              kiado: 'Wargaming',
-              free_to_play: 'igen'
-            },
-
-            {
-              nev: 'Simcity',
-              kiadasi_ev: 2013,
-              kiado: 'Electronic Arts',
-              free_to_play: 'nem'
-              
-            }
-        ]
-      }
   }
 }
 </script>
